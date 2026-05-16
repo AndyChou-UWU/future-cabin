b50011795ff61a161b9cef3de8cad49cee3510bb
unknown <you@example.com>
2026-05-16 23:41:04 +0800

    fix: explicit twins nested path for settings

--- smart_cabin_project/my_cabin/wsgi.py
+++ smart_cabin_project/my_cabin/wsgi.py
@@ -2,12 +2,7 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-# 終極大絕：強迫 Python 把所有資料夾層級全部加入搜尋路徑
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_DIR)
-sys.path.append(os.path.join(BASE_DIR, 'smart_cabin_project'))
-
 # 這樣不論它在內層還是外層，都能百分之百動態抓到 settings 檔案！
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smart_cabin_project.smart_cabin_project.settings')
 
 application = get_wsgi_application()
